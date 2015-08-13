7f90db4796c281dbc038bd39a98ee5fba5400b22
Stephane Nicoll <snicoll@pivotal.io>
2015-08-13 10:44:13 +0200

    Cache demonstration
    While our black box has some sort of caching of the Maven meta-data we
    can see in the logs that every time we need to diff two versions, it is
    called to recompute the model.
    
    We can add the cache starter and `@EnableCaching` on our app and it will
    just work out-of-the-box. Since we have no caching provider available,
    Spring Boot uses a simple implementation based on concurrent hash maps.
    
    We can also show how we can integrate a real cache library. For the sake
    of the demo, we're going to use ehcache 3 and create a
    JCacheManagerCustomizer that creates our `diffs` cache. Since the model
    may refer to a SNAPSHOT version, we need to expire the entry after some
    time. So we create a custom configuration that does that.
    
    Before restarting we also add our dear friend the actuator. Running our
    app again, we can see that the `/metrics` endpoint now shows hit/miss
    stats of our cache.

--- spring-boot-4tw-web/src/main/java/demo/config/Application.java
+++ spring-boot-4tw-web/src/main/java/demo/config/Application.java
@@ -1,13 +1,20 @@
 package demo.config;
 
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
@@ -17,6 +24,17 @@ public class Application {
 	@Bean
 	public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
 		return container -> container.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/400.html"));
+	}
+
+	@Bean
+	public JCacheManagerCustomizer cacheManagerCustomizer() {
+		return cm -> {
+			cm.createCache("diffs", new MutableConfiguration<>()
+					.setExpiryPolicyFactory(CreatedExpiryPolicy
+							.factoryOf(Duration.ONE_HOUR))
+					.setStoreByValue(false)
+					.setStatisticsEnabled(true));
+		};
 	}
 
 }
