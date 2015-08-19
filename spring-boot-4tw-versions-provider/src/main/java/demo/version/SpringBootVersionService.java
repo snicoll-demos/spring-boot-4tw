0dff8fe895410690c20579fe57a10447ff8c2524
Stephane Nicoll <snicoll@pivotal.io>
2015-08-19 10:09:09 +0200

    Switch to separate versions provider app
    Create a separate `spring-boot-4tw-versions-provider` application that
    provides the same feature as our extra controller providing the Spring
    Boot versions.
    
    This application can be deployed on CloudFoundry and the Angular app
    updated to showcase how it can connect to another service. Note that our
    extra app enable Spring 4.2's CORS support so that the browser accepts
    a connection to a separate service.

---
+++ spring-boot-4tw-versions-provider/src/main/java/demo/version/SpringBootVersionService.java
@@ -0,0 +1,47 @@
+package demo.version;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import demo.config.diff.support.AetherDependencyResolver;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringBootVersionService {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootVersionService.class);
+
+	private final AetherDependencyResolver dependencyResolver;
+
+	private final List<String> repositoryUrls;
+
+	public SpringBootVersionService() {
+		RemoteRepository[] repositories = new RemoteRepository[] {AetherDependencyResolver.SPRING_IO_RELEASE};
+		this.dependencyResolver = AetherDependencyResolver.create(false, repositories);
+		this.repositoryUrls = Arrays.asList(repositories).stream()
+				.map(RemoteRepository::getUrl).collect(Collectors.toList());
+	}
+
+	public List<String> getRepositoryUrls() {
+		return this.repositoryUrls;
+	}
+
+	@Cacheable("boot-versions")
+	public List<String> fetchBootVersions() {
+		try {
+			logger.info("Fetching Spring Boot versions from {}", repositoryUrls);
+			return dependencyResolver.resolveAvailableVersions("org.springframework.boot", "spring-boot");
+		}
+		catch (IOException e) {
+			throw new IllegalStateException("Failed to fetch Spring Boot versions", e);
+		}
+	}
+
+}
--- spring-boot-4tw-web/src/main/java/demo/config/Application.java
+++ spring-boot-4tw-web/src/main/java/demo/config/Application.java
@@ -4,18 +4,11 @@ import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
 
-import demo.config.springboot.SpringBootVersionService;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.health.AbstractHealthIndicator;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching
@@ -35,21 +28,6 @@ public class Application {
 					.setStatisticsEnabled(true);
 			cm.createCache("diffs", configuration);
 			cm.createCache("boot-versions", configuration);
-		};
-	}
-
-	@Bean
-	public HealthIndicator releaseRepositoryHealthIndicator(SpringBootVersionService springBootVersionService) {
-		return new AbstractHealthIndicator() {
-			@Override
-			protected void doHealthCheck(Health.Builder builder) throws Exception {
-				RestTemplate restTemplate = new RestTemplate();
-				for (String url : springBootVersionService.getRepositoryUrls()) {
-					ResponseEntity<String> entity = restTemplate
-							.getForEntity(url, String.class);
-					builder.up().withDetail(url, entity.getStatusCode());
-				}
-			}
 		};
 	}
 
