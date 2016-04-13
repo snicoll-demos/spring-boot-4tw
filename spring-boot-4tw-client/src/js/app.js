bclozel/spring-boot-4tw-steps

Auto-completion demonstration
We can further improve our application to offer an auto-completion of
available Spring Boot versions. To do that we update first our template
to invoke the proper function

```
typeahead="version for version in bootVersions | filter:$viewValue | limitTo:8
```

Instead of the hard-coded version we can create an extra endpoint that
provides the full list of versions which uses our existing
SpringBootVersionService. We can now call our service by filling the
`springBootServiceUrl` and calling our endpoint in the fetchBootVersions
function.

We can also improve our app to cache this result and we can showcase how
we can build a custom health indicator for the remote URLs we are using.

spring-boot-4tw-client/src/js/app.js
@@ -6,7 +6,7 @@ angular.module('diffApp', ['ngRoute', 'ui.bootstrap'])
     .factory('jQuery', ['$window', function ($window) {
         return $window.jQuery;
     }])
-    .constant('springBootVersionURL', '')
+    .constant('springBootVersionURL', '/springboot/versions.json')
     .config(['$provide', function ($provide) {
         $provide.value("apiRoot", $("head > base").attr("href"));
     }])
@@ -38,9 +38,7 @@ angular.module('diffApp', ['ngRoute', 'ui.bootstrap'])
             };
 
             var fetchBootVersions = function () {
-                var versions = $q.defer();
-                versions.resolve({data:['1.2.5', '1.2.6', '1.3.0.M4']});
-                return versions.promise;
+                return $http.get(springBootVersionURL);
             };
 
             return {

snicoll-demos/spring-boot-4tw

spring-boot-4tw-client/src/js/app.js
@@ -6,7 +6,7 @@ angular.module('diffApp', ['ngRoute', 'ui.bootstrap'])
     .factory('jQuery', ['$window', function ($window) {
         return $window.jQuery;
     }])
-    .value('springBootVersionURL', '')
+    .value('springBootVersionURL', '/springboot/versions.json')
 
     .config(function ($routeProvider) {
         $routeProvider
@@ -35,9 +35,7 @@ angular.module('diffApp', ['ngRoute', 'ui.bootstrap'])
             };
 
             var fetchBootVersions = function () {
-                var versions = $q.defer();
-                versions.resolve({data:['1.2.5', '1.2.6', '1.3.0.M4']});
-                return versions.promise;
+                return $http.get(springBootVersionURL);
             };
 
             return {